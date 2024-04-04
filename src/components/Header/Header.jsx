5cfbf7b2a33ccf0cdcb19a8e452268dadf497951
ferassmohana <ferassmoh@gmail.com>
2024-04-04 17:11:23 +0300

    Add MUI icons to userOperations

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,9 @@
 import styles from './Header.module.css';
+import { CustomInput } from "../CustomInput/CustomInput";
+
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 
 export const Header = () => {
     const linksArr = ["Handbags", "Watches", "Skincare", "Jewellery", "Apparels"];
@@ -17,14 +22,11 @@ export const Header = () => {
                 </div>
             </section>
             <section className={styles.opertaionsSection}>
-                <div className={styles.searchOperations}>
-                    {/* Here we will use icon */}
-                    <input type='search' />
-                </div>
+                <CustomInput id={"searchInput"} placeholder={"Search for products or brands....."} />
                 <div className={styles.userOperations}>
-                    {/* Here we will use icons as Links or Buttons */}
-                    {/* Here we will use icons as Links or Buttons */}
-                    {/* Here we will use icons as Links or Buttons */}
+                    <PersonOutlineIcon />
+                    <FavoriteBorderIcon />
+                    <ShoppingBagOutlinedIcon />
                 </div>
             </section>
         </header>
