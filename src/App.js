bfc39c6636596678e702fe6baa8eed2c174bb85a
ileen-Shaar <i26203332@gmail.com>
2024-04-04 23:29:50 +0300

    add button with style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Carousel from "./components/Carousel/Carousel";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Carousel />
+    </>
+  );
 }
 
 export default App;
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,8 +1,9 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import EastIcon from "@mui/icons-material/East";
 import SwipeableViews from "react-swipeable-views-react-18-fix";
 import { autoPlay } from "react-swipeable-views-utils";
 
@@ -10,33 +11,39 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    label: "Carry your Funk",
+    label: "Carry your Funk1",
     imgPath:
       "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
   },
   {
-    label: "Carry your Funk",
+    label: "Carry your Funk2",
     imgPath:
       "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
   },
   {
-    label: "Carry your Funk",
+    label: "Carry your Funk3",
     imgPath:
       "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
   },
   {
-    label: "Carry your Funk",
+    label: "Carry your Funk4",
     imgPath:
       "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
   },
 ];
 
 function Carousel() {
-  const [activeStep, setActiveStep] = React.useState(0);
+  const [activeStep, setActiveStep] = useState(0);
 
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
+  useEffect(() => {
+    const handleStepChange = (step) => {
+      setActiveStep(step);
+    };
+
+    return () => {
+      // Clean-up function if needed
+    };
+  }, [activeStep]);
 
   return (
     <Box
@@ -61,7 +68,7 @@ function Carousel() {
                   display: "block",
                   overflow: "hidden",
                   width: "100%",
-                  height: 275,
+                  height: 300,
                   fontWeight: 600,
                 }}
               >
@@ -93,13 +100,13 @@ function Carousel() {
                     sx={{
                       maxHeight: "70%",
                       overflow: "hidden",
-                      padding: "2% 3%",
+                      padding: "5% 4% 0",
                     }}
                   >
                     <Typography
                       fontSize={60}
                       fontWeight={"900"}
-                      lineHeight={"73px"}
+                      lineHeight={"50px"}
                     >
                       {step.label}
                     </Typography>
@@ -108,7 +115,7 @@ function Carousel() {
                       fontWeight={"400"}
                       lineHeight={"38px"}
                     >
-                      Trendy handbags collection for your party animal
+                      Trendy handbags collection for your party animal.
                     </Typography>
                   </Box>
                 </Paper>
@@ -117,6 +124,20 @@ function Carousel() {
           </div>
         ))}
       </AutoPlaySwipeableViews>
+      <Button
+        variant="contained"
+        startIcon={<EastIcon />}
+        sx={{
+          position: "absolute",
+          backgroundColor: "#1B4B66",
+          padding: "7px 40px",
+          left: "42%",
+          top: "70%",
+          borderRadius: "8px",
+        }}
+      >
+        See more
+      </Button>
     </Box>
   );
 }
