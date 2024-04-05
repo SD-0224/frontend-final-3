84306aae20a6722aa49c403b8e8a2d948d5fdeb8
ileen-Shaar <i26203332@gmail.com>
2024-04-05 21:57:34 +0300

    extract category titles to a component

--- src/components/carousel/carousel-items.jsx
+++ src/components/carousel/carousel-items.jsx
@@ -1,26 +1,14 @@
-import { Paper } from "@mui/material";
 import { IconicButton } from "../iconic-button";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import EastIcon from "@mui/icons-material/East";
 import { ProductImage } from "../product-image";
+import { CategoryItems } from "./category-titles";
 
 const imageContainerStyles = {
   position: "relative",
   display: "block",
   overflow: "hidden",
   fontWeight: 600,
-};
-
-const textContainerStyles = {
-  position: "absolute",
-  left: "40%",
-  width: "60%",
-  height: "80%",
-  top: "10%",
-  borderRadius: "15px 0 0 15px",
-  backgroundColor: "rgba(255, 255, 255, 0.5)",
-  color: "#1B4B66",
 };
 
 function CarouselItem(props) {
@@ -40,29 +28,10 @@ function CarouselItem(props) {
           }}
         >
           <ProductImage src={props.item.imgPath} width="100%" height={300} />
-          <Paper
-            square
-            elevation={0}
-            color="transparent"
-            sx={{
-              ...textContainerStyles,
-            }}
-          >
-            <Box
-              sx={{
-                maxHeight: "70%",
-                overflow: "hidden",
-                padding: "5% 4% 0",
-              }}
-            >
-              <Typography fontSize={60} fontWeight={"900"} lineHeight={"50px"}>
-                {props.item.name}
-              </Typography>
-              <Typography fontSize={28} fontWeight={"400"} lineHeight={"38px"}>
-                {props.item.description}
-              </Typography>
-            </Box>
-          </Paper>
+          <CategoryItems
+            title={props.item.name}
+            description={props.item.description}
+          />
         </Box>
 
         <Box sx={{ position: "absolute", left: "42%", top: "70%" }}>
