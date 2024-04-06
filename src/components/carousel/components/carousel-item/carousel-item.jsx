238655208353e5f04737a5837750ef21117710eb
ileen-Shaar <i26203332@gmail.com>
2024-04-06 20:00:29 +0300

    update custom button

--- src/components/carousel/components/carousel-item/carousel-item.jsx
+++ src/components/carousel/components/carousel-item/carousel-item.jsx
@@ -1,7 +1,6 @@
-import { IconicButton } from "../../../iconic-button/";
+import { CustomButton } from "../../../button";
 import { ProductImage } from "../../../product-image";
 import { CategoryTitles } from "../category-titles";
-import EastIcon from "@mui/icons-material/East";
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 
@@ -23,39 +22,40 @@ export function CarouselItem({ title, description, imgPath }) {
           borderRadius: "25px",
         }}
       >
-        {/* <Link to={"/"}> */}
-        <Box
-          sx={{
-            ...imageContainerStyles,
-          }}
-        >
-          <ProductImage src={imgPath} width="100%" height={300} />
-          <CategoryTitles title={title} description={description} />
-        </Box>
+        <Link to={"/"}>
+          <Box
+            sx={{
+              ...imageContainerStyles,
+            }}
+          >
+            <ProductImage src={imgPath} width="100%" height={300} />
+            <CategoryTitles title={title} description={description} />
+          </Box>
 
-        <Box sx={{ position: "absolute", left: "42%", top: "70%" }}>
-          <IconicButton
-            icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="11"
-                viewBox="0 0 16 11"
-                fill="none"
-              >
-                <path
-                  d="M1 5.49996H15M15 5.49996L10.5 1M15 5.49996L10.5 9.99996"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            }
-            label={"See More"}
-          />
-        </Box>
-        {/* </Link> */}
+          <Box sx={{ position: "absolute", left: "42%", top: "70%" }}>
+            <CustomButton
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="11"
+                  viewBox="0 0 16 11"
+                  fill="none"
+                >
+                  <path
+                    d="M1 5.49996H15M15 5.49996L10.5 1M15 5.49996L10.5 9.99996"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              }
+              label={"See More"}
+              variant={"contained"}
+            />
+          </Box>
+        </Link>
       </Box>
     </>
   );
