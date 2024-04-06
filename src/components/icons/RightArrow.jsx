afd3cf8175d9b5d9f46bb128e96fb9eea51fa663
MedoAngel <actionman14.ma@gmail.com>
2024-04-06 23:00:06 +0100

    add RightArrow to icons components

---
+++ src/components/icons/RightArrow.jsx
@@ -0,0 +1,24 @@
+export const RightArrow = function ({
+  width = 16,
+  height = 11,
+  borderColor = "#13101E",
+  borderSize = 2,
+  fillColor = "none",
+}) {
+  return (
+    <svg
+      {...{ width, height }}
+      viewBox="0 0 16 11"
+      fill={fillColor}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 5.49996H15M15 5.49996L10.5 1M15 5.49996L10.5 9.99996"
+        stroke={borderColor}
+        stroke-width={borderSize}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
