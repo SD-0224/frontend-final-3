77993a50d73e0c30630c3d9e140c27c684245c37
ferassmohana <ferassmoh@gmail.com>
2024-04-08 02:46:47 +0300

    Add css.module, add more expressive parametter

--- src/components/footer-list/FooterList.jsx
+++ src/components/footer-list/FooterList.jsx
@@ -1,27 +1,19 @@
 import { Box } from "@mui/material"
 import { List, ListItemButton, ListSubheader, ListItemText } from '@mui/material';
+import styles from "./FooterList.module.css";
 
-export const FooterList = ({ title, dataArr, type }) => {
-    const styles = {
-        lineHeight: "18px",
-        fontSize: 14,
-        fontWeight: 500,
-        textTransform: "capitalize",
-        paddingInline: "0px",
-        paddingBlock: "4px",
-        width: "100%"
-    }
+export const FooterList = ({ title, dataSource, type }) => {
     return (
         <Box sx={{ width: "fit-content" }}>
             <List>
-                <ListSubheader style={styles} sx={{ color: "#fff", background: "transparent", marginBottom: "8px" }}>
+                <ListSubheader className={styles.commenTextStyle} sx={{ color: "#fff", background: "transparent", marginBottom: "8px" }}>
                     {title}
                 </ListSubheader>
-                <Box sx={{ display: "flex",  flexWrap: "wrap", flexDirection: type === "row" ? "row" : "column", alignItems: type === "row" ? "center" : "baseline", gap: type === "row" ? "4px" : "0px" }}>
+                <Box sx={{ display: "flex", flexWrap: "wrap", flexDirection: type === "row" ? "row" : "column", alignItems: type === "row" ? "center" : "baseline", gap: type === "row" ? "4px" : "0px" }}>
                     {
-                        dataArr.map((data) => (
+                        dataSource.map((data) => (
                             <>
-                                <ListItemButton href="#" target="_blank" style={styles} sx={{ color: "#B6B6B6", '& .MuiListItemText-root': { margin: 0 }, flex: "none" }}>
+                                <ListItemButton href="#" target="_blank" className={styles.commenTextStyle} sx={{ color: "#B6B6B6", '& .MuiListItemText-root': { margin: 0 }, flex: "none" }}>
                                     <ListItemText sx={{ '& .MuiListItemText-primary': { fontSize: "14px" } }} primary={data} />
                                 </ListItemButton>
                                 {
