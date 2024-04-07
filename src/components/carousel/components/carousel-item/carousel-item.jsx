a3624f39d7b84fad545188a19ff37514b5689856
ileen-Shaar <i26203332@gmail.com>
2024-04-07 14:51:35 +0300

    update carousel icon to RightArrow component

--- src/components/carousel/components/carousel-item/carousel-item.jsx
+++ src/components/carousel/components/carousel-item/carousel-item.jsx
@@ -1,4 +1,5 @@
 import { CustomButton } from "../../../custom-button";
+import { RightArrow } from "../../../icons/RightArrow";
 import { ProductImage } from "../../../product-image";
 import { CategoryTitles } from "../category-titles";
 import { Box } from "@mui/material";
@@ -33,23 +34,7 @@ export function CarouselItem({ title, description, imgPath }) {
 
           <Box sx={{ position: "absolute", left: "42%", top: "70%" }}>
             <CustomButton
-              icon={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="11"
-                  viewBox="0 0 16 11"
-                  fill="none"
-                >
-                  <path
-                    d="M1 5.49996H15M15 5.49996L10.5 1M15 5.49996L10.5 9.99996"
-                    stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              }
+              icon={<RightArrow borderColor={"white"} />}
               label={"See More"}
               variant={"contained"}
             />
