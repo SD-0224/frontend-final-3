823b3a7981a183683a2c38be305b32a3da7783bc
ileen-Shaar <i26203332@gmail.com>
2024-04-07 20:18:24 +0300

    first pull request with fake data array

--- src/components/brand-section/brand-section.jsx
+++ src/components/brand-section/brand-section.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Typography, Paper, Box } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import { ProductImage } from "../product-image/ProductImage";
 
 const brands = [
@@ -16,6 +16,20 @@ const brands = [
   "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
   "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
 ];
+
+const brandContainer = {
+  display: "flex",
+  overflowX: "auto",
+  scrollbarWidth: "none",
+  msOverflowStyle: "none",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+  "& > :not(style)": {
+    mr: 6,
+    height: "187px",
+  },
+};
 
 export function BrandSection() {
   return (
@@ -35,24 +49,14 @@ export function BrandSection() {
         </Typography>
         <Box
           sx={{
-            display: "flex",
-            overflowX: "auto",
-            scrollbarWidth: "none",
-            msOverflowStyle: "none",
-            "&::-webkit-scrollbar": {
-              display: "none",
-            },
-            "& > :not(style)": {
-              mr: 6,
-              height: 187,
-            },
+            ...brandContainer,
           }}
         >
           {brands.map((brand) => {
             return (
-              <Paper key={brand.id} sx={{ minWidth: "168px" }}>
-                <ProductImage src={brand} width={"100%"} />
-              </Paper>
+              <div key={brand.id}>
+                <ProductImage src={brand} width={"168px"} />
+              </div>
             );
           })}
         </Box>
