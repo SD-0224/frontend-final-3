fddbf332d6c21a2aa5fecfb1bb5d2bea0d592755
ferassmohana <ferassmoh@gmail.com>
2024-04-08 01:07:26 +0300

    Add styles fot items

--- src/components/footer-list/FooterList.jsx
+++ src/components/footer-list/FooterList.jsx
@@ -2,17 +2,24 @@ import { Box } from "@mui/material"
 import { List, ListItemButton, ListSubheader, ListItemText } from '@mui/material';
 
 export const FooterList = ({ title, dataArr, type }) => {
+    const styles = {
+        lineHeight: "18px",
+        fontSize: 14,
+        fontWeight: 500,
+        textTransform: "capitalize",
+        paddingBlock: "0px"
+    }
     return (
         <Box>
             <List>
-                <ListSubheader>
+                <ListSubheader style={styles} sx={{ color: "#fff", background: "transparent", marginBottom: "8px" }}>
                     {title}
                 </ListSubheader>
                 <Box>
                     {
                         dataArr.map((data) => (
-                            <ListItemButton href="#" target="_blank"  >
-                                <ListItemText primary={data} />
+                            <ListItemButton href="#" target="_blank" style={styles} sx={{ color: "#B6B6B6", paddingBlock: 0 }}>
+                                <ListItemText sx={{ '& .MuiListItemText-primary': { fontSize: "14px" } }} primary={data} />
                             </ListItemButton>
                         ))
                     }
