d366c8bb2aa2bb41bb7dc87c024c9467ed743acf
ileen-Shaar <i26203332@gmail.com>
2024-04-10 20:57:19 +0300

    add right vector icon

---
+++ src/components/icons/RightVector.jsx
@@ -0,0 +1,26 @@
+export const RightVector = function ({
+  width = 24,
+  height = 24,
+  borderColor = "#13101E",
+  borderSize = 2,
+  fillColor = "none",
+  ...props
+}) {
+  return (
+    <svg
+      {...{ width, height }}
+      {...props}
+      viewBox="0 0 24 24"
+      fill={fillColor}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 4.5L16.5 12L9 19.5"
+        stroke={borderColor}
+        stroke-width={borderSize}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
