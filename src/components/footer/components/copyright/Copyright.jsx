5caf6be78d999400cb06d624c5f3f7285d724de7
ferassmohana <ferassmoh@gmail.com>
2024-04-11 23:03:23 +0300

    Build and finish copyright comp

--- src/components/footer/components/copyright/Copyright.jsx
+++ src/components/footer/components/copyright/Copyright.jsx
@@ -1,5 +1,13 @@
-export const Copyright = () => {
+import { Box } from "@mui/material";
+import CopyrightOutlinedIcon from '@mui/icons-material/CopyrightOutlined';
+export const Copyright = ({ text }) => {
+    const currentYear = new Date().getFullYear();
     return (
-        <></>
+        <Box sx={{ display: "flex", alignItems: "center", gap: "2px", color: "#B6B6B6" }}>
+            <CopyrightOutlinedIcon />
+            {currentYear}
+            <span>|</span>
+            <span>{text}</span>
+        </Box>
     )
 }
