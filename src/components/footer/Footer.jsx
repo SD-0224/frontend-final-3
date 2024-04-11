82205a2dc38b9853aef34e9d7b3d6086775756d5
ferassmohana <ferassmoh@gmail.com>
2024-04-11 23:55:35 +0300

    Add css module to make the code more readable, remove conditional css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,24 +4,14 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState, useEffect } from 'react';
 import { FooterList } from './components/footer-list/FooterList';
 import { Box } from "@mui/material";
-import { Facebook, Instagram, Twitter, Youtube, Location } from '../icons';
+import { Facebook, Instagram, Twitter, Youtube } from '../icons';
 import { CircleIconLink } from '../circle-icon-link';
-import CopyrightOutlinedIcon from '@mui/icons-material/CopyrightOutlined';
+import { CopyrightBox } from './components/copyright';
+import { LocationBox } from './components/location';
+import styles from './Footer.module.css';
+
 export const Footer = () => {
     const [isMobile, setIsMobile] = useState(false);
-    const currentYear = new Date().getFullYear();
-
-    const conditionalStyles = {
-        padding: isMobile ? "0px" : "30px 60px",
-        paddingBlock: "25px",
-        paddingInline: "16px",
-        gap: isMobile ? "24px" : "60px",
-        width: isMobile ? "100%" : "fit-content",
-        marginTop: isMobile ? "28px" : "0px",
-        borderTop: isMobile ? 1 : null,
-        borderColor: "#3E6F73",
-        alignItems: isMobile ? "baseline" : "end",
-    }
 
     const links = [
         {
@@ -91,13 +81,7 @@ export const Footer = () => {
 
 
     return (
-        <Accordion defaultExpanded={isMobile} sx={{
-            width: "100%",
-            margin: isMobile ? "0 !important" : "unset",
-            marginBottom: isMobile ? "unset !important" : "0",
-            marginTop: isMobile ? "auto !important" : "0",
-            boxShadow: "unset"
-        }}>
+        <Accordion defaultExpanded={isMobile} className={styles.accordion}>
 
             {
                 isMobile &&
@@ -110,44 +94,25 @@ export const Footer = () => {
                 </AccordionSummary>
             }
 
-            <AccordionDetails sx={{
-                background: "#1B4B66", color: "#fff", padding: conditionalStyles.padding, display: "flex", flexWrap: "wrap", justifyContent: "space-between"
-            }}>
+            <AccordionDetails className={styles.accordionDetails}>
 
-                <Box sx={{
-                    display: "flex", gap: conditionalStyles.gap, flexWrap: "wrap", width: conditionalStyles.width,
-                    paddingBlock: conditionalStyles.paddingBlock, paddingInline: conditionalStyles.paddingInline
-                }}>
+                <Box className={styles.listsContainer}>
                     <FooterList title={"Shop by Category"} links={links} />
                 </Box>
 
                 {/* Social media side */}
 
-                <Box sx={{
-                    display: "flex", flexDirection: "column", fontSize: "14px", alignItems: conditionalStyles.alignItems,
-                    gap: "8px", width: conditionalStyles.width, marginTop: conditionalStyles.marginTop,
-                    paddingBlock: conditionalStyles.paddingBlock, paddingInline: conditionalStyles.paddingInline,
-                    borderTop: conditionalStyles.borderTop, borderColor: conditionalStyles.borderColor
-                }}>
-                    <Box sx={{ display: "flex", gap: "16px", marginBottom: "14px", }}>
+                <Box className={`${styles.listsContainer} ${styles.socialContainer}`}>
+                    <Box className={styles.socialList}>
                         {
                             socialList?.map((social, index) => (
                                 <CircleIconLink key={index} link={social.link} icon={<social.iconName borderSize={0} fillColor={'#1b4b66'} />} />
                             ))
                         }
                     </Box>
-                    <Box sx={{ display: "flex", alignItems: "center", gap: "1px", color: "#fff" }}>
-                        <Location borderColor="#FFF" />
-                        <span>United States</span>
-                    </Box>
-                    <Box sx={{ display: "flex", alignItems: "center", gap: "2px", color: "#B6B6B6" }}>
-                        <CopyrightOutlinedIcon />
-                        {currentYear}
-                        <span>|</span>
-                        <span>Cora Leviene All Rights Reserved</span>
-                    </Box>
+                    <LocationBox country={"United States"} />
+                    <CopyrightBox text={"Cora Leviene All Rights Reserved"} />
                 </Box>
-
             </AccordionDetails>
         </Accordion>
     );
