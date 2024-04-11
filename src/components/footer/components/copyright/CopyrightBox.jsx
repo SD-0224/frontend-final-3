c5828ecb806705452b37c3a07644c4a5bd37f123
ferassmohana <ferassmoh@gmail.com>
2024-04-11 23:14:36 +0300

    update name of copyright to avoid duplicates with icon

--- src/components/footer/components/copyright/CopyrightBox.jsx
+++ src/components/footer/components/copyright/CopyrightBox.jsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import CopyrightOutlinedIcon from '@mui/icons-material/CopyrightOutlined';
-export const Copyright = ({ text }) => {
+export const CopyrightBox = ({ text }) => {
     const currentYear = new Date().getFullYear();
     return (
         <Box sx={{ display: "flex", alignItems: "center", gap: "2px", color: "#B6B6B6" }}>
