991eb4cbe09faaf5ffc562ff0b9a59ee6bbfb8c2
ferassmohana <ferassmoh@gmail.com>
2024-04-11 15:52:46 +0300

    Add social media icons and export it inside index.js

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,3 +2,7 @@ export * from "./RightArrow";
 export * from "./Heart";
 export * from "./ShoppingBag";
 export * from "./Profile";
+export * from "./Facebook";
+export * from "./Instagram";
+export * from "./Twitter";
+export * from "./Youtube";
