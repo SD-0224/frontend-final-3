2fb7747a86789f525c38408e26ba74657b3f233d
ferassmohana <ferassmoh@gmail.com>
2024-04-11 16:01:02 +0300

    Update import of Header comp inside App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Box from "@mui/material/Box";
-import { Header } from './components/Header';
+import { Header } from './components/header';
 import { Footer } from "./components/footer";
 import { MainRouter } from "./router";
 
