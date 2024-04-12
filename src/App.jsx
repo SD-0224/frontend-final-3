c206c49cf0f90a810ca9661cf15120fba0d71dd5
MedoAngel <actionman14.ma@gmail.com>
2024-04-12 15:47:14 +0100

    add container box around layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
 import "./App.css";
-import Box from "@mui/material/Box";
 import { Layout } from "./layout/Layout";
 
 function App() {
-  return (
-    <Box
-      sx={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Layout />
-    </Box>
-  );
+  return <Layout />;
 }
 
 export default App;
