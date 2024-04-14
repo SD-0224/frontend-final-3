e374dedfe9cc6fb5c37fff9d82618e68b5355b3b
ferassmohana <ferassmoh@gmail.com>
2024-04-14 20:35:27 +0300

    Add useRef to catch element, remove IDs, adjust functions

--- src/components/quantity-counter/QuantitiyCounter.jsx
+++ src/components/quantity-counter/QuantitiyCounter.jsx
@@ -1,26 +1,26 @@
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 import styles from './QuantitiyCounter.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
-export const QuantityCounter = ({quantityValue = 0}) => {
+export const QuantityCounter = ({ quantityValue = 0 }) => {
     const [value, setValue] = useState(quantityValue);
-
+    const decreseButton = useRef();
     const increaseHandler = () => {
-        setValue(value + 1);
+        setValue((value) => value + 1);
     }
 
     const decreaseHandler = () => {
-        setValue(value - 1);
+        setValue((value) => value - 1);
     }
 
     useEffect(() => {
         if (!value) {
-            document.getElementById("dec").style.pointerEvents = "none";
-            document.getElementById("dec").style.opacity = "0.2";
+            decreseButton.current.style.pointerEvents = "none";
+            decreseButton.current.style.opacity = "0.2";
         } else {
-            document.getElementById("dec").style.pointerEvents = "auto";
-            document.getElementById("dec").style.opacity = "1";
+            decreseButton.current.style.pointerEvents = "auto";
+            decreseButton.current.style.opacity = "1";
         }
     }, [value])
 
@@ -29,11 +29,11 @@ export const QuantityCounter = ({quantityValue = 0}) => {
         <div className={styles.container}>
             <span className={styles.quantityText}>Quantity:</span>
             <div className={styles.operations}>
-                <button className={styles.counterOperation} id='dec' type='button' onClick={decreaseHandler}>
+                <button className={styles.counterOperation} type='button' onClick={decreaseHandler} ref={decreseButton}>
                     <RemoveRoundedIcon />
                 </button>
                 <input type="number" id='quantityValue' value={value} />
-                <button className={styles.counterOperation} id='inc' type='button' onClick={increaseHandler}>
+                <button className={styles.counterOperation} type='button' onClick={increaseHandler}>
                     <AddRoundedIcon />
                 </button>
             </div>
