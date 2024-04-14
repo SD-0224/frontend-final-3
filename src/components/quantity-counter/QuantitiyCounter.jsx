25df1d1c1d5363a51438b2fb8ba4f980677d1841
ferassmohana <ferassmoh@gmail.com>
2024-04-14 18:59:35 +0300

    Add html elements of the component

--- src/components/quantity-counter/QuantitiyCounter.jsx
+++ src/components/quantity-counter/QuantitiyCounter.jsx
@@ -1,5 +1,19 @@
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
+
 export const QuantityCounter = () => {
     return (
-        <></>
+        <div>
+            <span>Quantity:</span>
+            <div>
+                <button id='inc'>
+                    <AddRoundedIcon />
+                </button>
+                <input type="number" />
+                <button id='dec'>
+                    <RemoveRoundedIcon />
+                </button>
+            </div>
+        </div>
     )
 }
