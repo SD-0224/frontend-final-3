a355493495f9c8ad8b22b0c633b89508bf47e0b8
ferassmohana <ferassmoh@gmail.com>
2024-04-14 19:09:49 +0300

    Add styles to reach the desired shape

--- src/components/quantity-counter/QuantitiyCounter.jsx
+++ src/components/quantity-counter/QuantitiyCounter.jsx
@@ -1,17 +1,18 @@
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
+import styles from './QuantitiyCounter.module.css';
 
 export const QuantityCounter = () => {
     return (
-        <div>
-            <span>Quantity:</span>
-            <div>
-                <button id='inc'>
-                    <AddRoundedIcon />
+        <div className={styles.container}>
+            <span className={styles.quantityText}>Quantity:</span>
+            <div className={styles.operations}>
+                <button id='dec' type='button'>
+                    <RemoveRoundedIcon />
                 </button>
                 <input type="number" />
-                <button id='dec'>
-                    <RemoveRoundedIcon />
+                <button id='inc' type='button'>
+                    <AddRoundedIcon />
                 </button>
             </div>
         </div>
