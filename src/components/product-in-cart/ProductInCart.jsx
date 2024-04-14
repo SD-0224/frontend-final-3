833701dc68240095a0b2b858788dc6501624a117
ferassmohana <ferassmoh@gmail.com>
2024-04-14 21:33:58 +0300

    Add the layout with comms and wait for other comp

--- src/components/product-in-cart/ProductInCart.jsx
+++ src/components/product-in-cart/ProductInCart.jsx
@@ -1,7 +1,30 @@
 import styles from './ProductInCart.module.css';
+import ProductImage from '../product-image';
 
-export const ProductInCart = () => {
+export const ProductInCart = ({ drawer }) => {
     return (
-        <></>
+        <div>
+            <div>
+                <ProductImage />
+                <div>
+                    <span></span>
+                    <span></span>
+                    {/* 
+                        Here will be condition for showing quantitiy-counter
+                        Here will be the quantity-counter 
+                     */}
+                    {/* 
+                        Here will be conditon for showing Qty- 1
+                        Here will be the span which hold the value
+                     */}
+                </div>
+            </div>
+            <div>
+                {/* 
+                    Here will be conditon for showing both remove-button & price 
+                    Here will be the remove and price     
+                */}
+            </div>
+        </div>
     )
 }
