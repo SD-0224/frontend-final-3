7799fb12cd100ee769763d3c9a9866ef6569e9d6
ferassmohana <ferassmoh@gmail.com>
2024-04-14 19:13:12 +0300

    Add handlers for changes in the value

--- src/components/quantity-counter/QuantitiyCounter.jsx
+++ src/components/quantity-counter/QuantitiyCounter.jsx
@@ -1,17 +1,28 @@
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 import styles from './QuantitiyCounter.module.css';
+import { useState } from 'react';
 
 export const QuantityCounter = () => {
+    const [value, setValue] = useState(0);
+
+    const increaseHandler = (event) => {
+        setValue(value + 1);
+    }
+
+    const decreaseHandler = (event) => {
+        setValue(value - 1);
+    }
+    
     return (
         <div className={styles.container}>
             <span className={styles.quantityText}>Quantity:</span>
             <div className={styles.operations}>
-                <button id='dec' type='button'>
+                <button id='dec' type='button' onClick={decreaseHandler}>
                     <RemoveRoundedIcon />
                 </button>
-                <input type="number" />
-                <button id='inc' type='button'>
+                <input type="number" value={value} />
+                <button id='inc' type='button' onClick={increaseHandler}>
                     <AddRoundedIcon />
                 </button>
             </div>
