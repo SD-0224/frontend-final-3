6c15358a0811bb9d639afd63ed6fde35589ea511
ileen-Shaar <i26203332@gmail.com>
2024-04-15 11:54:42 +0200

    delete tab button files & pass props correctly

--- src/components/payment-section/components/payment-component/PaymentComponent.jsx
+++ src/components/payment-section/components/payment-component/PaymentComponent.jsx
@@ -22,7 +22,7 @@ export function PaymentComponent({ label, src }) {
         onChange={handleChange}
         className={styles.input}
       ></input>
-      <img alt={`${label}`} src={{ src }} className={styles.paymentMethodImg} />
+      <img alt={label} src={src} className={styles.paymentMethodImg} />
       <Typography className={styles.paymentMethodText}>{label}</Typography>
     </Sheet>
   );
