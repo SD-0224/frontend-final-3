36c65c4baea3e5688de2523af6d2b4d1613953fd
ferassmohana <ferassmoh@gmail.com>
2024-04-15 22:14:21 +0300

    import quantity-counter, and pass parameters to it

--- src/components/product-in-cart/ProductInCart.jsx
+++ src/components/product-in-cart/ProductInCart.jsx
@@ -1,6 +1,7 @@
 import styles from './ProductInCart.module.css';
 import { ProductImage } from '../product-image';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { QuantityCounter } from '../quantity-counter';
 
 export const ProductInCart = ({ drawer = true, title = "Coach", subtitle = "Leather Coach Bag", price = "54.69", quantityValue = 1 }) => {
     return (
@@ -11,7 +12,7 @@ export const ProductInCart = ({ drawer = true, title = "Coach", subtitle = "Leat
                     <span className={styles.title}>{title}</span>
                     <span className={styles.subtitle}>{subtitle}</span>
                     {
-                        drawer && <></>
+                        drawer && <QuantityCounter quantityValue={quantityValue} showText={false} />
                     }
                     {
                         !drawer && <span>Qty- 1</span>
