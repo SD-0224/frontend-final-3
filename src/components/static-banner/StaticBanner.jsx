85c19b955fc30d1fd6ece06a34ced5adcd3134da
ileen-Shaar <i26203332@gmail.com>
2024-04-18 23:57:50 +0200

    convert to one component

--- src/components/static-banner/StaticBanner.jsx
+++ src/components/static-banner/StaticBanner.jsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material";
-import { DualCategoryRow } from "./components/dual-category-row";
-import { FullRowCategory } from "./components/full-row-category";
+import { DualCategoryRow } from "./components/category-row";
 
 const bannerContainer = {
   display: "flex",
   flexDirection: "column",
   justifyContent: "space-between",
+  gap: { xs: "20px", md: "40px" },
 };
 
 export function StaticBanner() {
@@ -15,31 +15,35 @@ export function StaticBanner() {
         ...bannerContainer,
       }}
     >
-      <FullRowCategory
-        title={"Limited Edition Products"}
+      <DualCategoryRow
+        variant="full"
         imageSrc={
           "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60"
         }
+        title="Limited Edition Products"
+        color="#97451F"
       />
 
       <Box display={"flex"} justifyContent={"space-between"}>
         <DualCategoryRow
+          variant="dual"
           imageSrc={
             "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60"
           }
           title="15% OFF & more"
           backgroundColor="#F1F1F1"
-          borderColor="#A53F64"
+          color="#A53F64"
           marginRight={{ xs: "20px", md: "32px" }}
         />
 
         <DualCategoryRow
+          variant="dual"
           imageSrc={
             "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60"
           }
           title="Popular in community!"
           backgroundColor="#C4C4C4"
-          borderColor="#1B4B66"
+          color="#1B4B66"
         />
       </Box>
     </Box>
