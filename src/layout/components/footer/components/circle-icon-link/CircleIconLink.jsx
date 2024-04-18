b6b64552db257bc50bce57d5b75376be75847f36
ileen-Shaar <i26203332@gmail.com>
2024-04-18 17:32:49 +0200

    add more props to circleIconLink component

--- src/layout/components/footer/components/circle-icon-link/CircleIconLink.jsx
+++ src/layout/components/footer/components/circle-icon-link/CircleIconLink.jsx
@@ -1,9 +1,23 @@
-import styles from './CircleIconLink.module.css';
+import styles from "./CircleIconLink.module.css";
 
-export const CircleIconLink = ({ link, icon, ...props }) => {
-    return (
-        <a href={link} rel="noreferrer" className={styles.circleLink} target="_blank" {...props}>
-            {icon}
-        </a>
-    )
-}
+export const CircleIconLink = ({
+  link,
+  icon,
+  height = "38px",
+  width = height,
+  backgroundColor = "#639599",
+  ...props
+}) => {
+  return (
+    <a
+      href={link}
+      rel="noreferrer"
+      className={styles.circleLink}
+      style={{ height, width, backgroundColor }}
+      target="_blank"
+      {...props}
+    >
+      {icon}
+    </a>
+  );
+};
