c56df9389d5588dca532a6885000fac5d4de9d8e
ileen-Shaar <i26203332@gmail.com>
2024-04-19 20:09:52 +0200

    change to desired style

--- src/components/tab-navigation/TabNavigation.jsx
+++ src/components/tab-navigation/TabNavigation.jsx
@@ -1,114 +1,79 @@
-import * as React from "react";
+import { useState } from "react";
 import { styled } from "@mui/system";
-import { buttonClasses } from "@mui/base/Button";
-import { Tabs } from "@mui/base/Tabs";
-import { Tab as BaseTab, tabClasses } from "@mui/base/Tab";
-import { TabsList as BaseTabsList } from "@mui/base/TabsList";
+import {
+  Tabs,
+  Tab as BaseTab,
+  tabClasses,
+  TabsList as BaseTabsList,
+} from "@mui/base";
 
-export function TabNavigation() {
+export function TabNavigation({
+  titles,
+  tabPadding = "6px 18px",
+  tabsPadding = "8px 16px",
+}) {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
-      <p>Selection following focus:</p>
       <Tabs
-        defaultValue={1}
-        aria-label="Tabs where selection follows focus"
+        onChange={handleChange}
+        defaultValue={0}
+        aria-label="Tabs Navigations"
         selectionFollowsFocus
       >
-        <TabsList>
-          <Tab value={1}>One</Tab>
-          <Tab value={2}>Two</Tab>
-          <Tab value={3}>Three</Tab>
-        </TabsList>
-      </Tabs>
-
-      <p>Selection independent of focus (default behavior):</p>
-      <Tabs
-        defaultValue={1}
-        aria-label="Tabs where selection does not follow focus"
-      >
-        <TabsList>
-          <Tab value={1}>One</Tab>
-          <Tab value={2}>Two</Tab>
-          <Tab value={3}>Three</Tab>
+        <TabsList padding={tabsPadding}>
+          {titles.map((title, index) => (
+            <Tab value={index} padding={tabPadding}>
+              {title}
+            </Tab>
+          ))}
         </TabsList>
       </Tabs>
     </div>
   );
 }
 
-const blue = {
-  50: "#F0F7FF",
-  100: "#C2E0FF",
-  200: "#80BFFF",
-  300: "#66B2FF",
-  400: "#3399FF",
-  500: "#007FFF",
-  600: "#0072E5",
-  700: "#0059B2",
-  800: "#004C99",
-  900: "#003A75",
-};
-
-const grey = {
-  50: "#F3F6F9",
-  100: "#E5EAF2",
-  200: "#DAE2ED",
-  300: "#C7D0DD",
-  400: "#B0B8C4",
-  500: "#9DA8B7",
-  600: "#6B7A90",
-  700: "#434D5B",
-  800: "#303740",
-  900: "#1C2025",
-};
-
 const Tab = styled(BaseTab)`
-  font-family: "IBM Plex Sans", sans-serif;
-  color: white;
+  color: var(--low-emphasis-text-color);
   cursor: pointer;
-  font-size: 0.875rem;
-  font-weight: bold;
+  font-size: 16px;
+  font-weight: var(--medium-font-weight);
   background-color: transparent;
-  width: 100%;
-  padding: 12px;
-  margin: 6px;
+  padding: ${({ padding }) => padding};
   border: none;
-  border-radius: 7px;
+  border-radius: 8px;
   display: flex;
   justify-content: center;
+  line-height: 20px;
 
   &:hover {
-    background-color: ${blue[400]};
+    background-color: #1b4b6611;
   }
 
   &:focus {
     color: #fff;
-    outline: 3px solid ${blue[200]};
   }
 
   &.${tabClasses.selected} {
-    background-color: #fff;
-    color: ${blue[600]};
-  }
-
-  &.${buttonClasses.disabled} {
-    opacity: 0.5;
-    cursor: not-allowed;
+    background-color: var(--primary);
+    color: var(--white);
   }
 `;
 
 const TabsList = styled(BaseTabsList)(
-  ({ theme }) => `
-  min-width: 400px;
-  background-color: ${blue[500]};
+  ({ theme, padding }) => `
+  background-color: var(--grey);
   border-radius: 12px;
-  margin-bottom: 16px;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   align-content: space-between;
-  box-shadow: 0px 4px 8px ${
-    theme.palette.mode === "dark" ? grey[900] : grey[200]
-  };
+  gap:24px;
+  padding:${padding};
   `
 );
