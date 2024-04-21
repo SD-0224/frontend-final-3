b0623a15e8aeb8b9713b9f16afe99036ff27d4d1
ferassmohana <ferassmoh@gmail.com>
2024-04-21 21:10:24 +0300

    Fininshing financial data

--- src/layout/components/header/components/financial-details/FinancialDetails.jsx
+++ src/layout/components/header/components/financial-details/FinancialDetails.jsx
@@ -1,5 +1,11 @@
-export const FinancialDetails = () => {
+import styles from './FinancialDetails.module.css'
+import { Box } from "@mui/material"
+
+export const FinancialDetails = ({ title, amount, total }) => {
     return (
-        <></>
+        <Box className={`${styles.financialDetails} ${total ? styles.total : null}`}>
+            <span>{title}:</span>
+            <span>${amount}</span>
+        </Box>
     )
 }
