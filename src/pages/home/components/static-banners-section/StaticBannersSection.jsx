9aefa4a011fba3eb836c9a6e06d464cbf324340f
MedoAngel <actionman14.ma@gmail.com>
2024-04-21 22:07:15 +0100

    add StaticBannersSection to home page components

---
+++ src/pages/home/components/static-banners-section/StaticBannersSection.jsx
@@ -0,0 +1,10 @@
+import { SectionContainer } from "../../../../components/section-container";
+import { StaticBanner } from "./components/static-banner";
+
+export const StaticBannersSection = function () {
+  return (
+    <SectionContainer paddingY="88px">
+      <StaticBanner />
+    </SectionContainer>
+  );
+};
