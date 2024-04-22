358f7ad1e862e7f84a8636f2b72ad67824359541
ferassmohana <ferassmoh@gmail.com>
2024-04-22 12:23:09 +0300

    updated the name of props to fit it's role

--- src/layout/components/header/components/financial-details/FinancialDetails.jsx
+++ src/layout/components/header/components/financial-details/FinancialDetails.jsx
@@ -1,9 +1,9 @@
 import styles from './FinancialDetails.module.css'
 import { Box } from "@mui/material"
 
-export const FinancialDetails = ({ title, amount, total }) => {
+export const FinancialDetails = ({ title, amount, isTotal }) => {
     return (
-        <Box className={`${styles.financialDetails} ${total ? styles.total : null}`}>
+        <Box className={`${styles.financialDetails} ${isTotal ? styles.total : null}`}>
             <span>{title}:</span>
             <span>${amount}</span>
         </Box>
