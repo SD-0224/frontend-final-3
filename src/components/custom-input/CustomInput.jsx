8609936782e85908364a16fdef330baf2a8ac23e
ileen-Shaar <i26203332@gmail.com>
2024-04-22 18:30:57 +0300

    add customInput props

--- src/components/custom-input/CustomInput.jsx
+++ src/components/custom-input/CustomInput.jsx
@@ -1,27 +1,42 @@
-import styles from './CustomInput.module.css';
+import styles from "./CustomInput.module.css";
 
-export const CustomInput = ({ label, id, type, placeholder, value, onChange, icon, ...props }) => {
-    return (
-        <div className={styles.inputWrapp}>
-            {label &&
-                <label htmlFor={id} className={styles.inputLabel}>
-                    {label}
-                </label>
-            }
-            <div className={styles.inputContainer}>
-                {icon &&
-                    <>
-                        {icon}
-                    </>
-                }
-                <input
-                    id={id}
-                    type={type}
-                    placeholder={placeholder}
-                    value={value}
-                    onChange={onChange}
-                />
-            </div>
-        </div>
-    )
-}
+export const CustomInput = ({
+  label,
+  id,
+  type,
+  placeholder,
+  value,
+  onChange,
+  icon,
+  borderRadius = "8px",
+  border = "0",
+  isGrey = false,
+  ...props
+}) => {
+  return (
+    <div className={styles.inputWrapp}>
+      {label && (
+        <label htmlFor={id} className={styles.inputLabel}>
+          {label}
+        </label>
+      )}
+      <div
+        className={styles.inputContainer}
+        style={{
+          borderRadius,
+          border,
+        }}
+      >
+        {icon && <>{icon}</>}
+        <input
+          className={isGrey ? styles.grey : null}
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/payment-section/components/payment-bottom/PaymentBottom.jsx
+++ src/components/payment-section/components/payment-bottom/PaymentBottom.jsx
@@ -3,6 +3,9 @@ import { Box, Typography } from "@mui/material";
 import { ProductImage } from "../../../product-image";
 import { CustomRadioButton } from "../../../custom-radio-button";
 import styles from "./PaymentBottom.module.css";
+import { CustomInput } from "../../../custom-input";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 export function PaymentBottom() {
   const [selectedValue, setSelectedValue] = useState("");
@@ -49,9 +52,48 @@ export function PaymentBottom() {
             Google Pay
           </Typography>
         </Box>
-        <Box
-          sx={{ display: "flex", flexDirection: "column", gap: "8px" }}
-        ></Box>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+          <CustomInput
+            id={"paymentId"}
+            placeholder={"Enter your UPI Id"}
+            type={"text"}
+            border={"1px solid var(--primary-tint)"}
+            borderRadius="4px"
+            isGrey={true}
+          />
+          <Typography
+            sx={{
+              fontSize: 12,
+              fontWeight: "var(--medium-font-weight)",
+              color: "#A7A7A7",
+            }}
+          >
+            Eg: 1234567890@ybl
+          </Typography>
+          <FormControlLabel
+            sx={{
+              marginLeft: "-3px",
+              display: "flex",
+              gap: "5px",
+              color: "var(--low-emphasis-text-color)",
+              fontWeight: "var(--medium-font-weight)",
+              fontSize: 14,
+            }}
+            control={
+              <Checkbox
+                defaultChecked
+                sx={{
+                  justifyContent: "start",
+                  padding: 0,
+                  "&.Mui-checked": {
+                    color: "var(--primary)",
+                  },
+                }}
+              />
+            }
+            label="Save this for future transactions"
+          />
+        </Box>
         <CustomRadioButton
           title={"Google Pay"}
           handleChange={handleChange}
