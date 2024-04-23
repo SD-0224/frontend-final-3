78f288c191e9bff7d08ec6c92069a76182abf4fe
MedoAngel <actionman14.ma@gmail.com>
2024-04-23 19:11:10 +0100

    add Home to pages

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../layout/Layout";
+import { Home } from "../pages/home";
 
 export const MainRouter = function () {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<h1>Home Page</h1>} />
+          <Route index element={<Home />} />
           <Route path="*" element={<h1>404 Page Not Found</h1>} />
         </Route>
       </Routes>
