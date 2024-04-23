73d03f22b1edbec2a05376eaa3d9449af839e9c0
ferassmohana <ferassmoh@gmail.com>
2024-04-23 18:54:48 +0300

    Build the main layout

--- src/components/products-table/ProductsTable.jsx
+++ src/components/products-table/ProductsTable.jsx
@@ -1,5 +1,29 @@
+import { ProductInCart } from '../product-in-cart';
+
 export const ProductsTable = () => {
-    return(
-        <></>
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Qty</th>
+                    <th>Subtotal</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><ProductInCart drawer={false} title={"Coach"} subtitle={"Leather Coach Bag"} quantity={1} /></td>
+                    <td>$54.69</td>
+                    <td>1</td>
+                    <td>
+                        <div>
+                            <span>$54.69</span>
+                            <button type="button">Remove</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     )
 }
