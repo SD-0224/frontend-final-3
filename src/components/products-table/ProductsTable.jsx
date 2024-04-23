4f1b978b055f2884b1313aeec950a51a4c8c8795
ferassmohana <ferassmoh@gmail.com>
2024-04-23 19:35:31 +0300

    Memoize and finish building table

--- src/components/products-table/ProductsTable.jsx
+++ src/components/products-table/ProductsTable.jsx
@@ -1,8 +1,27 @@
+import styles from './ProductsTable.module.css'
 import { ProductInCart } from '../product-in-cart';
+import { useMemo } from 'react';
 
-export const ProductsTable = () => {
+export const ProductsTable = ({ products }) => {
+
+    const memoizedProducts = useMemo(() => {
+        return products?.map((product, index) => (
+            <tr key={index}>
+                <td><ProductInCart drawer={false} title={product.title} subtitle={product.subtitle} quantity={product.quantity} /></td>
+                <td>${product.price}</td>
+                <td>{product.quantity}</td>
+                <td>
+                    <div className={styles.operations}>
+                        <span>${product.price}</span>
+                        <button type="button" className={styles.removeButton}>Remove</button>
+                    </div>
+                </td>
+            </tr>
+        ));
+    }, [products]);
+    
     return (
-        <table>
+        <table className={styles.table}>
             <thead>
                 <tr>
                     <th>Product Name</th>
@@ -12,17 +31,7 @@ export const ProductsTable = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td><ProductInCart drawer={false} title={"Coach"} subtitle={"Leather Coach Bag"} quantity={1} /></td>
-                    <td>$54.69</td>
-                    <td>1</td>
-                    <td>
-                        <div>
-                            <span>$54.69</span>
-                            <button type="button">Remove</button>
-                        </div>
-                    </td>
-                </tr>
+                {memoizedProducts}
             </tbody>
         </table>
     )
