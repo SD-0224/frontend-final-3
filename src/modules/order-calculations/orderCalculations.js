042115f51a0e559654414ed81853dddd0990ed79
ferassmohana <ferassmoh@gmail.com>
2024-04-24 23:42:24 +0300

    Add experssive variables naming, add more spaces to enhace readability

--- src/modules/order-calculations/orderCalculations.js
+++ src/modules/order-calculations/orderCalculations.js
@@ -1,19 +1,31 @@
+const DELIVERY_FEE = 12;
+const FIXED_DIGITES = 2;
+
 export const calcDiscount = ((products) => {
     let totalDisount = 0;
+
     products?.map((product) => {
-        return totalDisount += (product.price * product.discount) * (product.quantity / 100)
+        const finalPrice = product.price * product.discount;
+        const discountPercantage = product.discount / 100;
+
+        return totalDisount += finalPrice * discountPercantage;
     })
-    return totalDisount.toFixed(2);
+
+    return totalDisount.toFixed(FIXED_DIGITES);
 })
 
 export const calcSubTotal = ((products) => {
     let subTotal = 0;
+
     products?.map((product) => {
-        return subTotal += (product.price * product.quantity)
+        const finalPrice = product.price * product.discount;
+
+        return subTotal += finalPrice;
     })
-    return subTotal.toFixed(2);
+
+    return subTotal.toFixed(FIXED_DIGITES);
 });
 
 export const calcTotal = ((subTotal, discount) => {
-    return (Number(subTotal) - Number(discount) - 12).toFixed(2);
+    return (Number(subTotal) - Number(discount) - DELIVERY_FEE).toFixed(FIXED_DIGITES);
 })
