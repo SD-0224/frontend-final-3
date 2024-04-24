fca567a2691e11af4e7ebdf6775f651b08ea6cf3
MedoAngel <actionman14.ma@gmail.com>
2024-04-24 20:24:05 +0100

    redirect all requests to index.html to be handeld by react router

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { Home } from "../pages/home";
+import { About } from "../pages/about";
 
 export const MainRouter = function () {
   return (
@@ -8,6 +9,7 @@ export const MainRouter = function () {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<h1>404 Page Not Found</h1>} />
         </Route>
       </Routes>
