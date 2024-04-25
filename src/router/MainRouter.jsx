f723a8dd39e2da74c7cf767253373090f8db1044
ileen-Shaar <i26203332@gmail.com>
2024-04-25 15:22:00 +0300

    add about page to router

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { Home } from "../pages/home";
-import { MyCart } from '../pages/my-cart';
+import { MyCart } from "../pages/my-cart";
+import { About } from "../pages/about";
 
 export const MainRouter = function () {
   return (
@@ -10,6 +11,7 @@ export const MainRouter = function () {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/my-cart" element={<MyCart />} />
+          <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
