70c5f9347c11cc0a9c39ae78351a40f2ce3b6032
ileen-Shaar <i26203332@gmail.com>
2024-04-26 01:36:40 +0300

    add user profile page to router

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { Home } from "../pages/home";
 import { About } from "../pages/about";
-import { MyCart } from '../pages/my-cart';
+import { MyCart } from "../pages/my-cart";
+import { UserProfile } from "../pages/user-profile";
 
 export const MainRouter = function () {
   return (
@@ -12,6 +13,7 @@ export const MainRouter = function () {
           <Route index element={<Home />} />
           <Route path="/my-cart" element={<MyCart />} />
           <Route path="/about" element={<About />} />
+          <Route path="/user-profile" element={<UserProfile />} />
           <Route path="*" element={<h1>404 Page Not Found</h1>} />
         </Route>
       </Routes>
