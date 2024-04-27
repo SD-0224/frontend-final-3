771e465f610c5cc32bf26c9328393089de0505d2
ferassmohana <ferassmoh@gmail.com>
2024-04-27 14:38:04 +0300

    Add functionality to buttons, fix responsive issues

--- src/components/profile-sidebar/ProfileSidebar.jsx
+++ src/components/profile-sidebar/ProfileSidebar.jsx
@@ -3,6 +3,39 @@ import { Tabs, Box, Tab } from "@mui/material";
 import { RightChevron } from "../icons";
 import styles from "./ProfileSidebar.module.css";
 import { getSelectedColor } from "./components/tab-content-styles";
+import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 1 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
 
 export function SideBar({ SidebarOptions }) {
   const [value, setValue] = useState(0);
@@ -12,16 +45,39 @@ export function SideBar({ SidebarOptions }) {
   };
 
   const memoizedTabs = useMemo(() => {
+    const TabStyles = {
+      padding: "24px 10px",
+      maxWidth: "none",
+      '& .MuiTouchRipple-root': {
+        width: "3px",
+        height: "56px",
+        maxHeight: "100%",
+        background: "rgb(23, 73, 77)",
+        borderRadius: "8px",
+        transition: "0.2s all",
+        opacity: "0",
+        visibility: "hidden",
+        top: "50%",
+        transform: "translateY(-50%)",
+      },
+      '&.Mui-selected': {
+        '& .MuiTouchRipple-root': {
+          opacity: "1",
+          visibility: "visible",
+        }
+      },
+    };
     return SidebarOptions?.map((option, index) => (
       <Tab
         key={index}
-        sx={{ padding: "24px 10px", maxWidth: "none" }}
+        sx={TabStyles}
+        {...a11yProps(index)}
         label={
           <div
             className={`${styles.tabLabel}`}
             style={{ color: getSelectedColor(value, index) }}
           >
-            <span>{option}</span>
+            <span>{option.label}</span>
             <RightChevron borderColor={getSelectedColor(value, index)} />
           </div>
         }
@@ -30,19 +86,29 @@ export function SideBar({ SidebarOptions }) {
   }, [SidebarOptions, value]);
 
   return (
-    <Box
-      sx={{
-        bgcolor: "var(--grey)",
-        borderRadius: "10px",
-      }}
-    >
+    <Box sx={{
+      display: "grid", gridTemplateColumns: "280px 1fr", gap: "22px", alignItems: "start",
+      '@media (max-width: 768px)':
+        { '&': { gridTemplateColumns: "1fr" } }
+    }}>
       <Tabs
         orientation="vertical"
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs"
+        sx={{
+          borderRadius: "10px",
+          backgroundColor: "var(--grey)",
+          '@media (max-width: 768px)': {
+            '.MuiTabs-flexContainerVertical': {
+              flexDirection: "row",
+              overflow: "auto"
+            }
+          }
+        }}
         TabIndicatorProps={{
           style: {
+            display: "none",
             backgroundColor: "#17494D",
             width: "4px",
             left: 0,
@@ -54,6 +120,15 @@ export function SideBar({ SidebarOptions }) {
       >
         {memoizedTabs}
       </Tabs>
+      {
+        SidebarOptions?.map((option, index) => {
+          return (
+            <TabPanel value={value} index={index}>
+              {option.component}
+            </TabPanel>
+          )
+        })
+      }
     </Box>
   );
 }
