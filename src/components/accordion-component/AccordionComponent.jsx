ce8d98fb0249e0755f6f0dd00e9ccc64537fb50f
ferassmohana <ferassmoh@gmail.com>
2024-04-27 21:23:44 +0300

    remove uneeded div

--- src/components/accordion-component/AccordionComponent.jsx
+++ src/components/accordion-component/AccordionComponent.jsx
@@ -12,37 +12,31 @@ export function AccordionComponent({
   ...props
 }) {
   return (
-    <div>
-      <Accordion defaultExpanded={isExpanded} sx={{ boxShadow: 0 }} {...props}>
-        <AccordionSummary
-          expandIcon={
-            isExpandIcon && (
-              <ExpandMoreIcon sx={{ color: "var(--dark)", fontSize: "2rem" }} />
-            )
-          }
-          aria-controls="accordion-content"
-          sx={{
-            fontSize: "20px",
-            fontWeight: "var(--semi-bold-font-weight)",
-            padding: "0 0 8px",
-            borderBottom: 1,
-            borderColor: "rgba(0,0,0,0.12)",
-            my: "13px",
+    <Accordion defaultExpanded={isExpanded} sx={{ boxShadow: 0, '&::before': { display: 'none' } }} {...props}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon sx={{ color: "var(--dark)", fontSize: "2rem" }} />}
+        aria-controls="accordion-content"
+        sx={{
+          fontSize: "20px",
+          fontWeight: "var(--semi-bold-font-weight)",
+          padding: "0 0 8px",
+          borderBottom: 1,
+          borderColor: "rgba(0,0,0,0.12)",
+          my: "13px",
+          minHeight: "unset",
+          maxHeight: "36px",
+          ".MuiAccordionSummary-content.Mui-expanded": {
+            margin: 0,
+          },
+          "&.Mui-expanded": {
             minHeight: "unset",
             maxHeight: "36px",
-            ".MuiAccordionSummary-content.Mui-expanded": {
-              margin: 0,
-            },
-            "&.Mui-expanded": {
-              minHeight: "unset",
-              maxHeight: "36px",
-            },
-          }}
-        >
-          {title}
-        </AccordionSummary>
-        <AccordionDetails sx={{ padding: "0" }}>{children}</AccordionDetails>
-      </Accordion>
-    </div>
+          },
+        }}
+      >
+        {title}
+      </AccordionSummary>
+      <AccordionDetails sx={{ padding: "0" }}>{children}</AccordionDetails>
+    </Accordion>
   );
 }
