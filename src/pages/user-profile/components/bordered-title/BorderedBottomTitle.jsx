1be184869fe6c998522ea55a0876f86c6ef85527
ferassmohana <ferassmoh@gmail.com>
2024-04-27 15:21:25 +0300

    put span and hr inside div not h1?

--- src/pages/user-profile/components/bordered-title/BorderedBottomTitle.jsx
+++ src/pages/user-profile/components/bordered-title/BorderedBottomTitle.jsx
@@ -3,9 +3,9 @@ import styles from './BorderedBottomTitle.module.css';
 
 export const BorderedBottomTitle = ({ title, ...props }) => {
     return (
-        <h1 className={styles.BorderedBottomTitle} {...props}>
+        <div className={styles.BorderedBottomTitle} {...props}>
             <span>{title}</span>
             <Divider sx={{ marginTop: "5px" }} />
-        </h1>
+        </div>
     )
 }
