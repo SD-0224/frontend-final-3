572d06e86d7f0f9c4c82c1be62815e4d8e11091b
ileen-Shaar <i26203332@gmail.com>
2024-04-28 11:53:28 +0300

    fix static banner dual

--- src/pages/home/components/static-banners-section/components/static-banner/components/category-banner/CategoryBanner.jsx
+++ src/pages/home/components/static-banners-section/components/static-banner/components/category-banner/CategoryBanner.jsx
@@ -15,11 +15,11 @@ export function CategoryBanner({
 
   const dualTypo = {
     right: "35px",
-    top: { xs: "14px", sm: "18px", md: "22px", lg: "26px" },
-    fontSize: { xs: 20, sm: 25, md: 40, xl: 55 },
-    lineHeight: { xs: "20px", sm: "35px", md: "45px", lg: "52px" },
-    maxHeight: { xs: "50px", sm: "70px", md: "90px", lg: "104px" },
-    WebkitLineClamp: { xs: "2", md: "3" },
+    top: { xs: "40px", sm: "60px", md: "22px", lg: "26px" },
+    fontSize: { xs: 30, sm: 40, md: 35, lg: 40, xl: 55 },
+    lineHeight: { xs: "30px", sm: "40px", md: "45px", lg: "52px" },
+    maxHeight: { xs: "100px", sm: "120px", md: "90px", lg: "104px" },
+    WebkitLineClamp: 3,
     textAlign: "right",
   };
 
@@ -28,18 +28,18 @@ export function CategoryBanner({
     top: { xs: "40px", sm: "60px", md: "97px", lg: "147px" },
     fontSize: { xs: 30, sm: 40, md: 52 },
     lineHeight: { xs: "30px", sm: "40px", md: "60px", lg: "68px" },
-    maxHeight: { xs: "70px", sm: "90px", md: "110px", lg: "136px" },
-    WebkitLineClamp: 2,
+    maxHeight: { xs: "100px", sm: "120px", md: "110px", lg: "136px" },
+    WebkitLineClamp: 3,
   };
 
   const fullHeight = { xs: "200px", sm: "250px", md: "350px", lg: "400px" };
 
-  const dualHeight = { xs: "78px", sm: "128px", md: "178px", lg: "228px" };
+  const dualHeight = { xs: "200px", sm: "250px", md: "178px", lg: "228px" };
 
   return (
     <Box
       position={"relative"}
-      flex={isDual ? 1 : "none"}
+      flex={isDual ? { md: "1" } : "none"}
       height={isDual ? dualHeight : fullHeight}
       {...props}
     >
@@ -66,10 +66,8 @@ export function CategoryBanner({
       {isDual ? (
         <Box
           position={"absolute"}
-          top={{ xs: "10px", md: "auto" }}
-          left={{ xs: "10px", md: "auto" }}
-          right={{ xs: "auto", md: "26px" }}
-          bottom={{ xs: "auto", md: "10px", lg: "26px" }}
+          right={{ xs: "20px", md: "26px" }}
+          bottom={{ xs: "15px", md: "10px", lg: "26px" }}
         >
           <CircleIconLink
             icon={<RightArrow borderColor={color} width={31} height={21} />}
