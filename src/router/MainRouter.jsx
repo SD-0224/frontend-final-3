6050a2cbbd5ddf28b368ded79a93211ed3b9934e
MedoAngel <actionman14.ma@gmail.com>
2024-05-01 22:00:54 +0100

    add Category to pages

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { Home } from "../pages/home";
 import { About } from "../pages/about";
-import { MyCart } from '../pages/my-cart';
-import { UserProfile } from '../pages/user-profile';
+import { MyCart } from "../pages/my-cart";
+import { UserProfile } from "../pages/user-profile";
+import { Category } from "../pages/category";
 
 export const MainRouter = function () {
   return (
@@ -11,7 +12,9 @@ export const MainRouter = function () {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/category" element={<Category />} />
           <Route path="/my-cart" element={<MyCart />} />
+          <Route path="/user-profile" element={<UserProfile />} />
           <Route path="/about" element={<About />} />
           <Route path="/user-profile" element={<UserProfile />} />
           <Route path="*" element={<h1>404 Page Not Found</h1>} />
