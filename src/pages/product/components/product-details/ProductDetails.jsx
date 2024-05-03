b77d9d1a33e099558aea294b480cb2e9a0e95441
MedoAngel <actionman14.ma@gmail.com>
2024-05-03 16:28:50 +0100

    add spacing between elements in ProductDetails component

--- src/pages/product/components/product-details/ProductDetails.jsx
+++ src/pages/product/components/product-details/ProductDetails.jsx
@@ -1,12 +1,15 @@
 import { TitlesSection } from "./components/titles-section/TitlesSection";
 import { RatingsSection } from "./components/ratings-section/RatingsSection";
 import { PricingSection } from "./components/pricing-section/PricingSection";
+import { Box } from "@mui/system";
 
 export const ProductDetails = function ({ ...props }) {
   return (
     <>
       <TitlesSection {...props} />
-      <RatingsSection {...props} />
+      <Box sx={{ marginTop: "32px", marginBottom: "24px" }}>
+        <RatingsSection {...props} />
+      </Box>
       <PricingSection {...props} />
     </>
   );
