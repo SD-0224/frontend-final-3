6a30a0ecdd2178c6d3ced6f97f4fce22dbe3277b
MedoAngel <actionman14.ma@gmail.com>
2024-05-04 12:07:40 +0100

    add new arrival products to router on home

--- src/pages/home/components/new-arrivals-section/NewArrivalsSection.jsx
+++ src/pages/home/components/new-arrivals-section/NewArrivalsSection.jsx
@@ -1,18 +1,24 @@
 import { SectionContainer } from "../../../../components/section-container";
 import { HorizontalScroll } from "../../../../components/horizontal-scroll";
 import { NewArrivalsProductCard } from "./components/new-arrivals-product-card";
+import { Link } from "react-router-dom";
 
 export const NewArrivalsSection = function ({ products }) {
   return (
     <SectionContainer padding="0px">
       <HorizontalScroll title="New Arrivals">
         {products?.map((product) => (
-          <NewArrivalsProductCard
-            key={product.id}
-            image={product.smallImageUrl}
-            subtitle={product.shortSubtitle}
-            {...product}
-          />
+          <Link
+            to={`/product?productId=${product.id}`}
+            style={{ all: "unset", cursor: "pointer" }}
+          >
+            <NewArrivalsProductCard
+              key={product.id}
+              image={product.smallImageUrl}
+              subtitle={product.shortSubtitle}
+              {...product}
+            />
+          </Link>
         ))}
       </HorizontalScroll>
     </SectionContainer>
