1d3d92ffb7aab6497551b6042fd0e4e77773d9b7
ferassmohana <ferassmoh@gmail.com>
2024-05-04 13:10:28 +0300

    Fix the funcitonality of generating links

--- src/components/breadcrumbs/Breadcrumbs.jsx
+++ src/components/breadcrumbs/Breadcrumbs.jsx
@@ -4,14 +4,14 @@ import { useLocation, Link } from "react-router-dom";
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
 import styles from './breadcrumbs.module.css';
 
-export const Breadcrumbs = () => {
+export const Breadcrumbs = ({...props}) => {
     const URL = useLocation();
     const pathNamesArray = getPathNamesFromUrl(URL);
     let pathnamesHasItems = pathNamesArray.length > 0;
     return (
         <>
             {
-                pathnamesHasItems && <MUIBreadcrumbs aria-label="breadcrumb" separator={<ChevronRightRoundedIcon sx={{ color: "#171520" }} />} className={styles.breadcrumbs}>
+                pathnamesHasItems && <MUIBreadcrumbs {...props} aria-label="breadcrumb" separator={<ChevronRightRoundedIcon sx={{ color: "#171520" }} />} className={styles.breadcrumbs}>
                     <Link to={"/"}>Home</Link>
                     {
                         pathNamesArray?.map(link => <Link to={link.link}>{link.string}</Link>)
@@ -27,11 +27,13 @@ const getPathNamesFromUrl = (URL) => {
     let pathnamesLinks = URL["pathname"].split("/").filter((x) => x);
     let pathnamesStrings = pathnamesLinks.map(item => item.replaceAll("-", " "));
     let pathNames = [];
+    let urlString = "";
     if (URL.hash.length > 0) {
         pathnamesStrings[pathnamesStrings.length - 1] += URL.hash;
     }
     for (let i = 0; i < pathnamesLinks.length; i++) {
-        pathNames.push({ string: pathnamesStrings[i], link: pathnamesLinks[i] })
+        urlString += "/" + pathnamesLinks[i];
+        pathNames.push({ string: pathnamesStrings[i], link: urlString })
     }
 
     return pathNames;
