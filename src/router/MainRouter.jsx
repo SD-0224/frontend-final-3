897f998a0e96f6f8a4b94ed8405f12c278e88032
MedoAngel <actionman14.ma@gmail.com>
2024-05-04 01:03:14 +0100

    add Product page to router

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -3,9 +3,10 @@ import { Layout } from "../layout/Layout";
 import { Home } from "../pages/home";
 import { About } from "../pages/about";
 import { Category } from "../pages/category";
-import { MyCart } from '../pages/my-cart';
-import { UserProfile } from '../pages/user-profile';
-import { CheckoutPage } from '../pages/checkout';
+import { MyCart } from "../pages/my-cart";
+import { UserProfile } from "../pages/user-profile";
+import { CheckoutPage } from "../pages/checkout";
+import { Product } from "../pages/product";
 
 export const MainRouter = function () {
   return (
@@ -14,7 +15,8 @@ export const MainRouter = function () {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/category" element={<Category />} />
-          <Route path="/my-cart" element={<MyCart />} />          
+          <Route path="/product" element={<Product />} />
+          <Route path="/my-cart" element={<MyCart />} />
           <Route path="/user-profile/*" element={<UserProfile />} />
           <Route path="/about" element={<About />} />
           <Route path="/checkout" element={<CheckoutPage />} />
