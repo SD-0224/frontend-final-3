80c920dba759d3b5a521f77e84a2f3c108350734
MedoAngel <actionman14.ma@gmail.com>
2024-05-05 14:31:10 +0100

    remove router link from NewArrivalsSection

--- src/pages/home/components/new-arrivals-section/NewArrivalsSection.jsx
+++ src/pages/home/components/new-arrivals-section/NewArrivalsSection.jsx
@@ -1,24 +1,18 @@
 import { SectionContainer } from "../../../../components/section-container";
 import { HorizontalScroll } from "../../../../components/horizontal-scroll";
 import { NewArrivalsProductCard } from "./components/new-arrivals-product-card";
-import { Link } from "react-router-dom";
 
 export const NewArrivalsSection = function ({ products }) {
   return (
     <SectionContainer padding="0px">
       <HorizontalScroll title="New Arrivals">
         {products?.map((product) => (
-          <Link
+          <NewArrivalsProductCard
             key={product.id}
-            to={`/product?productId=${product.id}`}
-            style={{ all: "unset", cursor: "pointer" }}
-          >
-            <NewArrivalsProductCard
-              image={product.smallImageUrl}
-              subtitle={product.shortSubtitle}
-              {...product}
-            />
-          </Link>
+            image={product.smallImageUrl}
+            subtitle={product.shortSubtitle}
+            {...product}
+          />
         ))}
       </HorizontalScroll>
     </SectionContainer>
