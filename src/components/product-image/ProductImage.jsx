8e5735719af63f55ecb4de8a2210e1f06ffc0d7f
MedoAngel <actionman14.ma@gmail.com>
2024-05-05 18:27:29 +0100

    fix brands presentaion

--- src/components/product-image/ProductImage.jsx
+++ src/components/product-image/ProductImage.jsx
@@ -6,13 +6,24 @@ export const ProductImage = function ({
   width = "280px",
   height = width,
   maxWidth,
+  backgroundColor,
+  objectFit = "cover",
 }) {
   return (
-    <div style={{ width, height, flexShrink: 0, maxWidth }}>
+    <div
+      style={{
+        width,
+        height,
+        flexShrink: 0,
+        maxWidth,
+        backgroundColor,
+        borderRadius,
+      }}
+    >
       <img
         {...{ src }}
         className={styles.productImage}
-        style={{ borderRadius }}
+        style={{ borderRadius, objectFit }}
       />
     </div>
   );
